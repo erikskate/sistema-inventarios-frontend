99308cd769a0d9e9d8447ff555e77aa7e1cd8faf
erikskate <erikjoel35@hotmail.com>
2022-11-24 16:11:54 -0600

    Se agrega el menu del proyecto

--- src/app/modules/shared/sidenav/sidenav.component.ts
+++ src/app/modules/shared/sidenav/sidenav.component.ts
@@ -1,3 +1,4 @@
+import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  mobileQuery: MediaQueryList;
+
+  menuNav = [
+    {name:"Home", route: "home", icon: "home"},
+    {name:"Categorias", route: "home", icon: "category"},
+    {name:"Productos", route: "home", icon: "production_quantity_limits"}
+  ];
+
+  constructor(media: MediaMatcher) { 
+    this.mobileQuery = media.matchMedia('{max-width:600px}')
+  }
 
   ngOnInit(): void {
   }
